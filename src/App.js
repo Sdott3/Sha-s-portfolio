9787111a6af8935ffa0482604501d37842fbbf6a
sdott3 <sdotthomas347@gmail.com>
2022-11-04 19:14:16 -0400

    updated needed files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "./components/Navbar/Navbar";
+
+export default function App () {
+    return (
+        <div>
+            <Nav />
+        </div>
+    );
+}

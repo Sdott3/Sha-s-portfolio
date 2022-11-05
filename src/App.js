4be37d48eb69c7cf3517744d2ea9463e4936cfad
sdott3 <sdotthomas347@gmail.com>
2022-11-04 21:03:38 -0400

    added about me  to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./components/Navbar/Navbar";
+import About from "./components/About/About";
 
 export default function App () {
     return (
         <main className="text-gray-400 bg-gray-900 body-font">
             <Nav />
+            <About />
         </main>
     );
 }
